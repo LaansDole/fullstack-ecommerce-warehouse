7624e4092e9dcdb37d68562743f957d1d4e2c8b6
dolelongan <dolelongan@gmail.com>
2024-03-14 19:56:00 +0700

    feature: add necessary routes

--- server-go/go.mod
+++ server-go/go.mod
@@ -5,10 +5,12 @@ go 1.22.1
 require (
 	github.com/gin-contrib/cors v1.7.0
 	github.com/gin-gonic/gin v1.9.1
+	github.com/go-sql-driver/mysql v1.8.0
 	github.com/joho/godotenv v1.5.1
 )
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/bytedance/sonic v1.11.2 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
 	github.com/chenzhuoyu/iasm v0.9.1 // indirect
--- server-go/models/db.go
+++ server-go/models/db.go
@@ -1,1 +1,65 @@
 package models
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+)
+
+var (
+	DBAdmin  *sql.DB
+	DBBuyer  *sql.DB
+	DBSeller *sql.DB
+)
+
+func init() {
+
+	godotenv.Load()
+	var err error
+
+	DBAdmin, err = sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8",
+		os.Getenv("DB_USER_ADM"),
+		os.Getenv("DB_PASS_ADM"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("MYSQL_DB"),
+	))
+	if err != nil {
+		log.Fatalf("Error on initializing database connection: %s", err.Error())
+	}
+
+	DBBuyer, err = sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8",
+		os.Getenv("DB_USER_BUYER"),
+		os.Getenv("DB_PASS_BUYER"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("MYSQL_DB"),
+	))
+	if err != nil {
+		log.Fatalf("Error on initializing database connection: %s", err.Error())
+	}
+
+	DBSeller, err = sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8",
+		os.Getenv("DB_USER_SELLER"),
+		os.Getenv("DB_PASS_SELLER"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("MYSQL_DB"),
+	))
+	if err != nil {
+		log.Fatalf("Error on initializing database connection: %s", err.Error())
+	}
+
+	DBAdmin.SetConnMaxLifetime(100)
+	DBAdmin.SetMaxIdleConns(10)
+	DBAdmin.SetMaxOpenConns(10)
+
+	DBBuyer.SetConnMaxLifetime(100)
+	DBBuyer.SetMaxIdleConns(10)
+	DBBuyer.SetMaxOpenConns(10)
+
+	DBSeller.SetConnMaxLifetime(100)
+	DBSeller.SetMaxIdleConns(10)
+	DBSeller.SetMaxOpenConns(10)
+}
