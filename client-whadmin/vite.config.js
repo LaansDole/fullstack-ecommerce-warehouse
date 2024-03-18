69940e6180c9fd06cc279e4e9486d9dc47c7a5e3
dolelongan <dolelongan@gmail.com>
2024-03-18 23:41:31 +0700

    update: prepare for deployment

--- client-whadmin/vite.config.js
+++ client-whadmin/vite.config.js
@@ -1,10 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        "axios", // ignore react stuff
+      ],
+    }
+  },
   server: {
     port: 3002,
   },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    }
+  }
 });
